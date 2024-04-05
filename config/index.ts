fba594c556896e33d24ec04f1236e12cb9c32a6b
AdewuyiDaniels <adewuyiaby@gmail.com>
2024-04-05 12:43:08 +0100

    Added images to the Index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,44 +1,39 @@
 export const PRODUCT_CATEGORIES = [
   {
-    label: 'UI Kits',
-    value: 'ui_kits' as const,
+    label: 'Premium Rice Varieties',
+    value: 'premium_rice' as const,
     featured: [
       {
-        name: 'Editor picks',
-        href: `/products?category=ui_kits`,
-        imageSrc: '/nav/ui-kits/mixed.jpg',
+        name: 'Long Grain Rice',
+        description: 'Rich in nutrients and wholesome goodness',
+        href: '/products?category=premium_rice&product=organic_brown',
+        imageSrc: 'public\LongGrainRice.jpg',
       },
       {
-        name: 'New Arrivals',
-        href: '/products?category=ui_kits&sort=desc',
-        imageSrc: '/nav/ui-kits/blue.jpg',
-      },
-      {
-        name: 'Bestsellers',
-        href: '/products?category=ui_kits',
-        imageSrc: '/nav/ui-kits/purple.jpg',
+        name: 'Short Grain Rice',
+        description: 'Perfect for Asian desserts and savory dishes',
+        href: '/products?category=premium_rice&product=glutinous_sticky',
+        imageSrc: 'public\ShortGrainRice.jpg',
       },
     ],
   },
   {
-    label: 'Icons',
-    value: 'icons' as const,
+    label: 'Wholesome Beans Selection',
+    value: 'beans' as const,
     featured: [
       {
-        name: 'Favorite Icon Picks',
-        href: `/products?category=icons`,
-        imageSrc: '/nav/icons/picks.jpg',
+        name: 'Pinto Beans',
+        description: 'Creamy texture and versatile in cooking',
+        href: '/products?category=beans&product=pinto',
+        imageSrc: 'public/HoneyBeans.jpg',
       },
       {
-        name: 'New Arrivals',
-        href: '/products?category=icons&sort=desc',
-        imageSrc: '/nav/icons/new.jpg',
-      },
-      {
-        name: 'Bestselling Icons',
-        href: '/products?category=icons',
-        imageSrc: '/nav/icons/bestsellers.jpg',
+        name: 'Adzuki Red Beans',
+        description: 'Sweet flavor and ideal for desserts',
+        href: '/products?category=beans&product=adzuki_red',
+        imageSrc: 'public\MilkBeans.jpg',
       },
     ],
   },
-]
+  
+];
